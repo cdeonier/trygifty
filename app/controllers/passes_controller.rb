b88888ba125cc5e8308feff8c0e251a94cfdb2d2
cdeonier <cdeonier@gmail.com>
2012-11-09 15:22:23 -0800

    Destroying passes remove pkpass from amazon s3

--- app/controllers/passes_controller.rb
+++ app/controllers/passes_controller.rb
@@ -4,8 +4,17 @@ class PassesController < ApplicationController
   end
   
   def destroy
-    @pass = Pass.find(params[:id])
-    @pass.destroy
+    pass = Pass.find(params[:id])
+    vendor = Vendor.find(pass.vendor.id)
+    
+    AWS::S3::DEFAULT_HOST.replace "s3-us-west-1.amazonaws.com"
+    AWS::S3::Base.establish_connection!(
+        :access_key_id     => 'AKIAJZORP2CG2ZKHVMJQ',
+        :secret_access_key => 'sK+LaL59L8BWY1beskIGBAaLSrjglJB3fw7Oyc2T')
+        
+    AWS::S3::S3Object.delete "passes/#{vendor.biz_id}/#{pass.serial_number}.pkpass", "gifty"
+    
+    pass.destroy
     
     redirect_to passes_path
   end
