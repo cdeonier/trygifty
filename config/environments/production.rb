ff007331a15bdb4ee6c607df325251d844cbb4ff
cdeonier <cdeonier@gmail.com>
2012-11-10 23:27:30 -0800

    Trying gmail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Gifty::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :authentication       => :plain,
+    :port                 => 587,
+    :domain               => 'mail.trygifty.com',
+    :user_name            => 'orders@trygifty.com',
+    :password             => 'orders-pickle123',
+    :enable_starttls_auto => true  }
 end
